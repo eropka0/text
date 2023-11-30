f425cd96419d828697f1cad513f98172fee23d54
eropka0 <65648779+eropka0@users.noreply.github.com>
2023-11-30 15:30:43 +0900

    Add files via upload

---
+++ Text-editor/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfTextEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File|*.txt|Word Document|*.doc";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string fileExtension = Path.GetExtension(filePath);
+                if (fileExtension == ".txt")
+                {
+                    File.WriteAllText(filePath, textInputTextBox.Text);
+                }
+                else if (fileExtension == ".doc")
+                {
+                    File.WriteAllText(filePath, textInputTextBox.Text);
+                }
+                MessageBox.Show("File saved to " + filePath);
+            }
+        }
+    }
+}
